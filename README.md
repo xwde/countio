af433eb063ea2a919908eff7e2e432bd6ddc4606
Oleh Martsokha <o.martsokha@gmail.com>
2023-02-28 01:57:26 +0100

    feat-docs (#5)

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 ## xwde: countio
 
-[![Build Status][action-badge]][action-url]
+[![Build Status][action-badge]][action-url] [![Docs][docs-badge]][docs-url]
 [![Crate Version][crates-badge]][crates-url]
 
 [action-badge]: https://img.shields.io/github/actions/workflow/status/xwde/countio/build.yaml?branch=main&label=build&logo=github&style=for-the-badge
 [action-url]: https://github.com/xwde/countio/actions/workflows/build.yaml
 [crates-badge]: https://img.shields.io/crates/v/countio.svg?logo=rust&style=for-the-badge
 [crates-url]: https://crates.io/crates/countio
+[docs-badge]: https://img.shields.io/docsrs/countio?logo=Docs.rs&style=for-the-badge
+[docs-url]: http://docs.rs/countio
 
 The wrapper struct to enable byte counting for `std::io::Read` and
 `std::io::Write` and its asynchronous variants from `futures` and `tokio`
@@ -15,13 +17,15 @@ crates.
 > **Note** : The library contains unsafe code. See implementation of `Async*`
 > traits for more details.
 
-Following features available:
+### Features
 
 - `std` to enable `std::io::Read` and `std::io::Write`. **Enabled by default**.
 - `futures` to enable `futures_io::AsyncRead` and `futures_io::AsyncWrite`.
 - `tokio` to enable `tokio::io::AsyncRead` and `tokio::io::AsyncWrite`.
 
-### `std::io::Read`:
+### Examples
+
+- `std::io::Read`:
 
 ```rust
 use std::io::prelude::*;
@@ -36,11 +40,11 @@ fn main() {
     let mut buf = String::new();
     let len = reader.read_line(&mut buf).unwrap();
 
-    assert_eq!(len, reader.get_ref().read_bytes());
+    assert_eq!(len, reader.get_ref().reader_bytes());
 }
 ```
 
-### `std::io::Write`:
+- `std::io::Write`:
 
 ```rust
 use std::io::prelude::*;
@@ -56,6 +60,6 @@ fn main() {
     let len = writer.write(buf).unwrap();
     writer.flush().unwrap();
 
-    assert_eq!(len, writer.get_ref().written_bytes());
+    assert_eq!(len, writer.get_ref().writer_bytes());
 }
 ```
--- counter/mod.rs
+++ counter/mod.rs
@@ -1,17 +1,65 @@
-mod common;
-pub use common::*;
+/// Generic reader & writer wrapper with byte counting capability.
+pub struct Counter<D> {
+    pub(crate) inner: D,
+    pub(crate) reader_bytes: usize,
+    pub(crate) writer_bytes: usize,
+}
 
-#[cfg(feature = "std")]
-mod stdlib;
-#[cfg(feature = "std")]
-pub use self::stdlib::*;
+impl<D> Counter<D> {
+    /// Creates a new `Counter<D>` with a zero read/written bytes.
+    pub fn new(inner: D) -> Self {
+        Self::with_bytes(0, 0, inner)
+    }
+
+    /// Creates a new `Counter<D>` with the specified read and written bytes.
+    pub fn with_bytes(reader_bytes: usize, writer_bytes: usize, inner: D) -> Self {
+        Self {
+            inner,
+            reader_bytes,
+            writer_bytes,
+        }
+    }
+
+    /// Returns the sum of read and written bytes by the underlying reader/writer.
+    pub fn total_bytes(&self) -> usize {
+        self.reader_bytes + self.writer_bytes
+    }
+
+    /// Returns the count of read bytes by the underlying reader.
+    pub fn reader_bytes(&self) -> usize {
+        self.reader_bytes
+    }
+
+    /// Returns the count of written bytes by the underlying writer.
+    pub fn writer_bytes(&self) -> usize {
+        self.writer_bytes
+    }
+
+    /// Consumes `Counter` returning the underlying reader/writer.
+    pub fn into_inner(self) -> D {
+        self.inner
+    }
+
+    /// Gets a reference to the underlying reader/writer.
+    pub fn get_ref(&self) -> &D {
+        &self.inner
+    }
+
+    /// Gets a mutable reference to the underlying reader/writer.
+    pub fn get_mut(&mut self) -> &mut D {
+        &mut self.inner
+    }
+}
+
+impl<D> From<D> for Counter<D> {
+    fn from(inner: D) -> Self {
+        Self::new(inner)
+    }
+}
 
 #[cfg(feature = "futures")]
 mod futures;
-#[cfg(feature = "futures")]
-pub use self::futures::*;
-
+#[cfg(feature = "std")]
+mod stdlib;
 #[cfg(feature = "tokio")]
 mod tokio;
-#[cfg(feature = "tokio")]
-pub use self::tokio::*;
